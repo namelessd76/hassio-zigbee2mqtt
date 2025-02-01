1aecbfe7965bbddb6311b63b8abf5c4058a265ea
PDV <d.pogodin76@gmail.com>
2025-02-01 23:30:44 +0200

    Proper docker settings

--- common/local_build.sh
+++ common/local_build.sh
@@ -1,8 +1,1 @@
-cd ..
-cp common/Dockerfile zigbee2mqtt-edge/
-cp -R common/rootfs zigbee2mqtt-edge/
-docker run --rm --privileged \
--v $(pwd)/zigbee2mqtt-edge:/data homeassistant/amd64-builder --amd64 -t /data \
---no-cache
-rm -rf zigbee2mqtt-edge/rootfs
-rm zigbee2mqtt-edge/Dockerfile
+ docker buildx build --platform linux/amd64,linux/arm64 -t namelessd76/zigbee2mqtt-aarch64:edge . --push
